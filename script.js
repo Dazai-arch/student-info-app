6c5f21ca1ce248b8f26aa58585f9ba8aa49d2c64
Alok Tayal <aloktayal2003@gmail.com>
2026-09-07 10:21:40 +0530

    Initial student information app

---
+++ script.js
@@ -0,0 +1,4 @@
+document.getElementById("showDetailsBtn").addEventListener("click", function () {
+  document.getElementById("detailsOutput").textContent =
+    "Details: John, Register No. 101, Programme MCA";
+});
